a283f677404e92f0d10585f2378554fe82a04b3c
Zeus Online <replit1@never.ga>
2022-06-09 16:33:38 +0530

    second commit

---
+++ matmul.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class matmul{
+
+    public static void main(String[] args){
+
+        int[][] a = new int[3][2];
+        int[][] b = new int[2][3];
+        int[][] c = new int[3][3];
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int i =0; i<3;i++){
+                for (int j=0; j<2; j++){
+                    a[i][j] = sc.nextInt();
+                }
+            }
+            
+            for (int i =0; i<2;i++){
+                for (int j=0; j<3; j++){
+                    b[i][j] = sc.nextInt();
+                }
+            }
+        }
+
+        for (int i =0; i<3;i++){
+            for (int j=0; j<3; j++){
+                c[i][j] = 0;
+                for (int k=0; k<2; k++){
+                    c[i][j] += a[i][k]*b[k][j];
+                }
+            }
+        }
+        
+
+        for (int i =0; i<3;i++){
+            for (int j=0; j<3; j++){
+                System.out.print(c[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+}
